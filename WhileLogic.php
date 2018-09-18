644eae091b404df7488e6a0c2c1acdf06b331dbe
Unknown <EBoblett949@west-mec.org>
2018-09-18 14:01:50 -0700

    Added While Logic
    Added While Logic

--- WhileLogic.php
+++ WhileLogic.php
@@ -11,13 +11,20 @@
      -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Is Even</title>
+    <title>While Logic</title>
     <script src="modernizr.custom.65897.js"></script>
 </head>
 <body>
 <h2>While Logic</h2>
     <?php
         $counter = 0;
+        while ($counter <= 100) {
+            $numbers[] = $counter;
+            ++$counter;
+        }
+        foreach ($numbers as $currentNum) {
+            echo "$currentNum<br>";
+        }
     ?>
 </body>
 </html>
